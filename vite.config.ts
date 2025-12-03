1302c6cb3749b0a25ad157a400e89f2c4d15210a
MD-Abdul-Raheem <your-email@example.com>
2025-12-03 23:01:45 +0530

    Fix vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: repo name must match EXACTLY your GitHub repo
 export default defineConfig({
   plugins: [react()],
-  base: '/Workout-Tracker/', 
+  base: '/',
 })
